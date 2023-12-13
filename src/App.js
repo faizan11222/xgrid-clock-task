325c5ee033bbba099c82ce5918e79b8983060152
faizan <faizandevp@gmail.com>
2023-12-13 13:16:57 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./assets/css/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="clock-body">
+        <div className="hours">
+          <div className="minutes">
+            <div className="hours-pin">
+              <div className="minutes-pin"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
